c59fc5575c33b79f42b58ee0f948db1c1a3fda93
Harpreet Gaur <harpreet.gaur97@gmail.com>
2019-06-12 15:37:59 -0400

    Answers to command line assignment #1

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -4,19 +4,21 @@
 #
 
 # count the number of unique stations
-
+cut -d, -f5,9 201402-citibike-tripdata.csv | sort | uniq | wc -l
 # count the number of unique bikes
-
+cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq | wc -l
 # count the number of trips per day
-
+cut -d, -f2 201402-citibike-tripdata.csv | cut -d' ' -f2 | sort | uniq -c
 # find the day with the most rides
-
+cut -d, -f2 201402-citibike-tripdata.csv | cut -d' ' -f2 | sort | uniq -c| sort | tail -n1 
 # find the day with the fewest rides
+cut -d, -f2 2014-citibike-tripdata.csv | cut -d' ' -f2 | sort | head -n2
 
 # find the id of the bike with the most rides
-
+cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq -c | sort -nr | head -n1
 # count the number of rides by gender and birth year
-
+cut -d, -f14,15 2014-citibike-tripdata.csv | sort | uniq -c
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
-
+cut -d, -f5 201402citibike-tripdata.csv | grep '[0-9].* & .*[0-9].*' | sort | wc -l
 # compute the average trip duration
+awk -F, '{gsub(/"/, "",$1); sum+=$1;k++} END {print sum/k}' 201402-citibike-tripdata.csv
